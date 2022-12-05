ec614719aef76bd2cfe2af08f7295868d01c9160
Marelyn Deschamps <mdeschampsfranco@gmail.com>
2022-12-05 15:21:22 -0400

    First commit

--- js/main.js
+++ js/main.js
@@ -6,22 +6,32 @@ enviar.onclick = getPersona;
 let borrar = document.getElementById("borrar");
 borrar.onclick = deletContent;
 
+let body = document.getElementById("body");
 
 
 function getPersona(){
    
 
     const nombre = document.getElementById("nombreOid__input").value;
-    const  url = `https://jsonplaceholder.typicode.com/posts/${nombre}`; 
+    
+    if(nombre == ""){
 
+        fetch(`https://jsonplaceholder.typicode.com/posts/`)
+        .then((response) => response.json())
+        .then((data) => {showPersonas(data)});
+    }
    
+
+    else{
+        const  url = `https://jsonplaceholder.typicode.com/posts/${nombre}`; 
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {showPersona(data)});
+    }
  
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {showPersona(data)});
-     
  
      }
+
  
     function showPersona(persona){
      deletContent();
@@ -56,8 +66,16 @@ function getPersona(){
      
     }
 
+function showPersonas(persona){
+    let cuerpo1 = '';
+    for(let i = 0; i<persona.length; i++){
+        cuerpo1 += `<tr><td>${persona[i].id} <td>${persona[i].title}</td><td> ${persona[i].body}</td></tr>`;
+    }
+    body.innerHTML = cuerpo1;
+
+}
+
 
 function deletContent(){
     container.innerHTML = " ";
-    nombrePlano.innerHTML = " ";
 }
